8aab6a7eb9143eaee439971a8c63e7e1681d5544
marco borra <mborraborra40@gmail.com>
2025-06-21 09:30:39 +0200

    Pulizia e inserimento codice completo con favicon temporanea

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="it">
+      <Head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="application-name" content="HVAC AI App" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
